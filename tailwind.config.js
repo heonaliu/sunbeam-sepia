9f8799ee9bdc41820b6c949505885c3042e9b000
Heona Liu <75399515+heonaliu@users.noreply.github.com>
2026-08-29 11:08:52 -0400

    install tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+// tailwind.config.js
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        slate: "#586080",
+        ice: "#D8ECF7",
+        sky: "#A4DAEC",
+        blue: "#4FA8D6",
+        navy: "#124A7D",
+        rose: "#DD8AA1",
+        sand: "#E7AC63",
+      },
+      fontFamily: {
+        pixel: ['"Press Start 2P"', "cursive"],
+      },
+      boxShadow: {
+        // stepped "pixel" border look, no blur/rounded corners
+        pixel: "4px 4px 0px 0px #124A7D",
+        "pixel-sm": "2px 2px 0px 0px #124A7D",
+      },
+      borderRadius: {
+        none: "0px", // pixel art = no rounded corners anywhere
+      },
+    },
+  },
+  plugins: [],
+};

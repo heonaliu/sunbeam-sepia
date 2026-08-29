6ef930164a1ed6883e9351a5dc2fb75a5cb93edb
Heona Liu <75399515+heonaliu@users.noreply.github.com>
2026-08-29 16:37:26 -0400

    landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,16 +5,26 @@ export default function LandingPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="min-h-screen bg-ice flex flex-col items-center justify-center gap-6">
-      <img
-        src={sepiaImg}
-        alt="Sepia"
-        className="w-64 sm:w-80 md:w-96 mx-auto"
-      />
-      <button onClick={() => navigate("/capture")} className="btn-pixel-rose">
-        START
-      </button>
-      <p className="text-navy">capture the day, the people, the memories</p>
+    <div className="min-h-screen bg-ice flex flex-col">
+      <div className="flex-1 flex items-center gap-10 px-16">
+        <div className="flex flex-col items-start gap-6">
+          <img
+            src={sepiaImg}
+            alt="Sepia"
+            className="w-96 sm:w-[28rem] md:w-[32rem]"
+          />
+          <button
+            onClick={() => navigate("/capture")}
+            className="btn-pixel-rose w-[50rem] -ml-40 text-right"
+          >
+            START
+          </button>
+        </div>
+
+        <div className="flex-1" />
+      </div>
+
+      <p className="text-navy text-center pb-8">made by Heona Liu, Jana Leung, Emma Deeken</p>
     </div>
   );
 }
